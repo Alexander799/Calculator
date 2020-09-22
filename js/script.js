01c051cd804afcc86ad9c256ee56caf5bdc2cab2
Alexander799 <sashkakovalev63@mail.com>
2020-09-22 11:49:06 +0300

    writing calculator logic

--- js/script.js
+++ js/script.js
@@ -4,21 +4,24 @@ var num1, num2, operator, operatorTemp = /^[0-9]{0,20}[\+\-\*\/]{1}[0-9]{0,20}$/
 
 function initial(n) {
     inputConcat += usInput[n].textContent;
-    if (inputConcat.length < 3) {
+    if (inputConcat.indexOf('Delete') != (-1)) {
+        document.getElementById('result').innerHTML = '0';
+    } else if (inputConcat.indexOf('Backspace') != (-1)) {
+        inputConcat.slice(inputConcat.length - 1);
         document.getElementById('result').innerHTML = inputConcat;
     } else {
-        valiNum(inputConcat);
+        document.getElementById('result').innerHTML = inputConcat;
     }
+
+
+    document.getElementById('test3').innerHTML = inputConcat.indexOf('Delete') != (-1);
+    document.getElementById('test').innerHTML = inputConcat;
+    document.getElementById('test2').innerHTML = operatorTemp.test(inputConcat);
 }
 
-function valiNum(inNum) {
-    this.inNum = inNum;
-    if (inNum == operatorTemp) {
 
-    }
-}
 /*
-document.getElementById('result');
+
 1.получаем значение от ввода
 2.цикл проходится по строке и проверяет ее длину. Если длина меньше 3, то выходим из усл. оператора.
   Если значение >=3, то запускается второй цикл, который проходит по строке и ищет индекс оператора
